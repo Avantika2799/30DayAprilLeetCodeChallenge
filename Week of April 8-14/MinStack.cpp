f0e97110560741caed4ad5eb37103fc4196a5c18
AVANTIKA SHARMA <43870344+Avantika2799@users.noreply.github.com>
2020-05-04 16:51:23 +0530

    10th April

---
+++ Week of April 8-14/MinStack.cpp
@@ -0,0 +1,38 @@
+/*
+https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3292/
+*/
+
+class MinStack {
+public:
+    /** initialize your data structure here. */
+        vector<int>v;
+    int mini_elem;
+    MinStack() {
+        mini_elem=INT_MAX;
+    }
+
+    void push(int x) {
+        v.push_back(x);
+    }
+    
+    void pop() {
+         v.pop_back();
+    }
+    
+    int top() {
+        return v[v.size()-1];
+    }
+    
+    int getMin() {
+        return *min_element(v.begin(),v.end());
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */

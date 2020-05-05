70e5b96bbf8018e8ddf79a9d9d178ca7a3801606
AVANTIKA SHARMA <43870344+Avantika2799@users.noreply.github.com>
2020-05-05 20:13:35 +0530

    22nd April

---
+++ Week of April 22-28/SubarraySumEqualsK.cpp
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/531/week-4/3307/
+*/
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count=0;
+        unordered_map<int,int>m;
+        int sum=0;
+        m[0]=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+           if(m.find(sum-k)!=m.end())
+               
+           {
+               count+=m[sum-k];
+           }
+            m[sum]++;
+        }
+        
+        return count;
+    }
+};

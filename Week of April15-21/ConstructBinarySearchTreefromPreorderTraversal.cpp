c478b6ecdeb103c57f8f5d73b3c16982eb990b6f
AVANTIKA SHARMA <43870344+Avantika2799@users.noreply.github.com>
2020-05-05 19:26:27 +0530

    20th April

---
+++ Week of April15-21/ConstructBinarySearchTreefromPreorderTraversal.cpp
@@ -0,0 +1,35 @@
+/*
+https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/530/week-3/3305/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+     TreeNode* constructBinTree(TreeNode *root, int &cval) {
+        if(root==NULL) {
+            root=new TreeNode(cval);
+            
+        } else if(cval < root->val) {
+            root->left = constructBinTree(root->left, cval);
+        } else if( cval> root->val) {
+            root->right = constructBinTree(root->right, cval);
+        }
+        return root;
+        
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        TreeNode *root = constructBinTree(root,preorder[0]);;
+        for(int i = 1; i<preorder.size() ;i++) {
+            constructBinTree(root,preorder[i]);
+        }
+        return root;
+    }
+};
